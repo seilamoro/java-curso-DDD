82054820347b1f84d2b5f3133a74de419f13bca2
sheila <sheila.moro.herrero@gmail.com>
2020-05-07 17:22:02 +0200

    modelando el dominio 1: 1-	UUIDs como identificadores

---
+++ src/shop/main/s/moro/shop/categories/domain/CategoryId.java
@@ -0,0 +1,12 @@
+package s.moro.shop.categories.domain;
+
+import s.moro.shared.domain.Identifier;
+
+import java.util.UUID;
+
+public class CategoryId extends Identifier {
+
+    public CategoryId(String value) {
+        super(value);
+    }
+}
